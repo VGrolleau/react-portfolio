db1cb71f3d13d97b6ba83fb92bc60344ee55cfc6
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-04-26 15:26:25 +0200

    Review layout content and style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,9 +3,17 @@ import './index.scss';
 
 const Layout = () => {
     return (
-        <>
+        <div className="App">
             <Sidebar />
-        </>
+            <div className="page">
+                <span className="tags top-tags">&lt;body&gt;</span>
+                <span className="tags bottom-tags">
+                    &lt;/body&gt;
+                    <br />
+                    <span className="bottom-tag-html">&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
     )
 };
 
