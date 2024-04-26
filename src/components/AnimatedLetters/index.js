409862cbaf89af75979fee8bc7b5c95b777c0870
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-04-26 18:37:23 +0200

    Review home text style with animation

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+const AnimatedLetters = ({ letterClass, charactersArray, index }) => {
+    return (
+        <span>
+            {
+                charactersArray.map((character, i) => (
+                    <span key={character + i} className={`${letterClass} _${i + index}`}>
+                        {character}
+                    </span>
+                ))
+            }
+        </span>
+    )
+};
+
+export default AnimatedLetters;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,35 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const hiArray = ['B', 'o', 'n', 'j', 'o', 'u', 'r', ','];
+    const iAmArray = ['J', 'e', ' ', 's', 'u', 'i', 's'];
+    const nameArray = ['i', 'r', 'g', 'i', 'n', 'i', 'e', ','];
+    const jobArray = ['d', 'é', 'v', 'e', 'l', 'o', 'p', 'p', 'e', 'u', 's', 'e', ' ', 'w', 'e', 'b', ' ', 'a', 'l', 't', 'e', 'r', 'n', 'a', 'n', 't', 'e', '.'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 6500)
+    }, [])
+
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                    Bonjour, <br /> Je suis
+                    <AnimatedLetters letterClass={letterClass} charactersArray={hiArray} index={10} />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass} charactersArray={iAmArray} index={18} />
                     <svg xmlns="http://www.w3.org/2000/svg" className="img-home">
                         <text x="0" y="50" fontFamily="Coolvetica" fill="#ffd700" className='text-v-home'>V</text>
                         <text x="3" y="48" fontFamily="Coolvetica" fill="#115173" className='text-v-home'>V</text>
                     </svg>
-                    irginie,
+                    <AnimatedLetters letterClass={letterClass} charactersArray={nameArray} index={25} />
                     <br />
-                    développeuse web alternante
+                    <AnimatedLetters letterClass={letterClass} charactersArray={jobArray} index={33} />
                 </h1>
                 <h2>Développeuse Fullstack / Spécialisée ReactJS</h2>
                 <Link to="/contact" className="flat-button">ME CONTACTER</Link>
