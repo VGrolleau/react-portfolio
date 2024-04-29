7a7aaa4ec357495ab34babc4deabbd777de11fa8
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-04-29 21:50:28 +0200

    Finish spinning cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCss3, faGitAlt, faHtml5, faJs, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+import { faCss3Alt, faGitAlt, faHtml5, faJsSquare, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -33,21 +33,21 @@ const About = () => {
             </div>
 
             <div className='stage-cube-cont'>
-                <div className='cubespiner'>
+                <div className='cubespinner'>
                     <div className='face1'>
                         <FontAwesomeIcon icon={faHtml5} color="#F06529" />
                     </div>
                     <div className='face2'>
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
                     </div>
                     <div className='face3'>
-                        <FontAwesomeIcon icon={faSass} color="#CF649A" />
+                        <FontAwesomeIcon icon={faCss3Alt} color="#28A4D9" />
                     </div>
                     <div className='face4'>
-                        <FontAwesomeIcon icon={faJs} color="#EFD81D" />
+                        <FontAwesomeIcon icon={faSass} color="#CF649A" />
                     </div>
                     <div className='face5'>
-                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
                     </div>
                     <div className='face6'>
                         <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
