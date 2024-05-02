806ba4ed54570b0e89baab9bf21d8e92d5ee7df3
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-05-02 16:00:47 +0200

    Link contact form to emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,34 @@
 import Loader from 'react-loaders';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const titleArray = ['M', 'e', ' ', 'c', 'o', 'n', 't', 'a', 'c', 't', 'e', 'r'];
+
+    const refForm = useRef();
 
     useEffect(() => {
         setTimeout(() => {
             return setLetterClass('text-animate-hover')
         }, 2500)
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm("service_vgrolleau", "template_vgrolleau", refForm.current, "PRikGgLu4Y4aBE_WI")
+            .then(() => {
+                console.log('SUCCESS!');
+                alert('Message envoyé avec succès !');
+                window.location.reload(false);
+            }, (error) => {
+                console.log('FAILED...', error);
+                alert("Échec de l'envoi du message, recommencez s'il vous plaît");
+            });
+    };
 
     return (
         <>
@@ -26,13 +43,13 @@ const Contact = () => {
                     </p>
 
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
-                                    <input type='text' name='name' placeholder='Nom' required />
+                                    <input type='text' name='user_name' placeholder='Nom' required />
                                 </li>
                                 <li className='half'>
-                                    <input type='email' name='email' placeholder='Email' required />
+                                    <input type='email' name='user_email' placeholder='Email' required />
                                 </li>
                                 <li>
                                     <input type='text' name='subject' placeholder='Objet' required />
