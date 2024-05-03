e04ea3321e070419cf0cb0abfb8bf97983461f9e
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-05-03 13:58:48 +0200

    Add animated title

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,11 +1,26 @@
 import Loader from 'react-loaders';
 import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Skills = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const titleArray = ['C', 'o', 'm', 'p', 'é', 't', 'e', 'n', 'c', 'e', 's', ' ', '&', ' ', 'E', 'x', 'p', 'é', 'r', 'i', 'e', 'n', 'c', 'e', 's'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 3600)
+    }, []);
+
     return (
         <>
             <div className='container skills-page'>
-                <div className='text-zone'></div>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} charactersArray={titleArray} index={10} />
+                    </h1>
+                </div>
                 <div className='skills-charts'></div>
             </div>
             <Loader type='pacman' />
