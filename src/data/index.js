a645407f34211798e8d7f31ce3b0b20d815a985d
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-05-09 12:09:30 +0200

    Start add data projects

--- src/data/index.js
+++ src/data/index.js
@@ -125,3 +125,451 @@ export const dataCanvasGeneral = [
         url: 'https://www.npmjs.com/'
     }
 ];
+
+export const dataProjects = [
+    {
+        category: {
+            personnel: [
+                {
+                    name: "La bataille",
+                    description: {
+                        text: "Projet de jeu de carte « La bataille » réalisé à titre personnel. Permet de jouer contre l’ordinateur à une version simplifiée de la bataille avec un jeu de 32 cartes.",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "HTML5",
+                        "CSS3",
+                        "JavaScript"
+                    ],
+                    links: {
+                        github: {
+                            frontend: 'https://github.com/VGrolleau/war-card-game',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: 'https://virginiegrolleau.com/sites/jeux/war-card-game/',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "Promenade en forêt",
+                    description: {
+                        text: "Projet d’histoire dont vous êtes le héros réalisé à titre personnel. Elle permet de s’évader dans une histoire fantastique unique où on se promène en forêt. Mais il arrive qu’en empruntant un chemin ou un autre on puisse se retrouver bien ennuyés…",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "HTML5",
+                        "CSS3",
+                        "JavaScript",
+                        "Écriture d’une histoire",
+                        "Réflexion sur la cartographie de la promenade"
+                    ],
+                    links: {
+                        github: {
+                            frontend: 'https://github.com/VGrolleau/forest-game',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: 'https://virginiegrolleau.com/sites/jeux/forest-game/',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "Casse-briques",
+                    description: {
+                        text: "Projet de jeu du casse-briques réalisé à titre personnel. Possibilité de mettre en pause le jeu, de couper et remettre le son.La raquette est manipulable à la souris comme au clavier(touches fléchées).",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "HTML5",
+                        "CSS3",
+                        "JavaScript",
+                        "Algorithmique"
+                    ],
+                    links: {
+                        github: {
+                            frontend: 'https://github.com/VGrolleau/casse-briques',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: 'https://virginiegrolleau.com/sites/jeux/casse-briques/',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "Memory panda",
+                    description: {
+                        text: "Projet personnel de jeu de mémoire sous forme de paires de cartes à retrouver avec des animaux comme personnages. Comprend un compte des points lorsque les paires sont assemblées, un décompte lorsqu’on se trompe, et contient une surprise musicale lors de la victoire.",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "HTML5",
+                        "CSS3",
+                        "JavaScript",
+                        "Formulaire",
+                        "Intégration de vidéo youtube dans une fenêtre modale"
+                    ],
+                    links: {
+                        github: {
+                            frontend: 'https://github.com/VGrolleau/memory-panda',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: 'https://virginiegrolleau.com/sites/jeux/memory-panda/',
+                            other: ''
+                        }
+                    }
+                }
+            ],
+            openclassrooms: [
+                {
+                    name: "HRNet",
+                    description: {
+                        text: "Projet basé sur une application web interne qui gère les dossiers des employés d’une grande société financière. L’objectif est de mettre à jour le site, à savoir le passer de JQuery à React. Voici un récapitulatif des principales tâches:",
+                        li: [
+                            {
+                                classname: "first-level",
+                                content: "Convertir l’ensemble du projet en React."
+                            },
+                            {
+                                classname: "first-level",
+                                content: "Convertir l’un des quatre plugins jQuery actuels en React. Remplacer les 3 plugins jQuery restants par des composants React soit en les codant, soit en les important depuis des libraires existantes."
+                            },
+                            {
+                                classname: "first-level",
+                                content: "Effectuer des tests de performance Lighthouse en comparant l’ancienne et la nouvelle application."
+                            }
+                        ]
+                    },
+                    skills: [
+                        "Passage d’une application JQuery en React",
+                        "Choix du design le plus approprié en fonction du logo",
+                        "Système de gestion d’état Redux",
+                        "Mise en place d’un Back-End avec Express, Sequelize et base de données MariaDB",
+                        "Transformation de plugins JQuery (sélection de date, fenêtre modale et tableau) en React via des composants existants",
+                        "Transformation du plugin JQuery de menu déroulant en React via sa création",
+                        "Publication du plugin créé sur npm"
+                    ],
+                    links: {
+                        github: {
+                            frontend: 'https://github.com/VGrolleau/p14-hrnet-v2-front',
+                            backend: 'https://github.com/VGrolleau/p14-hrnet-v2-back',
+                            other: 'https://github.com/VGrolleau/dropdown-menu-component'
+                        },
+                        site: {
+                            app: '',
+                            other: 'https://www.npmjs.com/package/dropdown-menu-component'
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                },
+                {
+                    name: "",
+                    description: {
+                        text: "",
+                        li: [
+                            {
+                                classname: "",
+                                content: ""
+                            }
+                        ]
+                    },
+                    skills: [
+                        "",
+                        "",
+                        ""
+                    ],
+                    links: {
+                        github: {
+                            frontend: '',
+                            backend: '',
+                            other: ''
+                        },
+                        site: {
+                            app: '',
+                            other: ''
+                        }
+                    }
+                }
+            ]
+        }
+    }
+]
