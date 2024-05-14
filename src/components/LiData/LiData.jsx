ef60322a388d426c9a3c97cfd85323df559cb5a3
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-05-14 17:42:20 +0200

    Start working on getting modal data

--- src/components/LiData/LiData.jsx
+++ src/components/LiData/LiData.jsx
@@ -2,8 +2,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './LiData.scss';
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 const LiData = ({ category, dataObject, activeLiIndex, setActiveLiIndex, setAnimationKey }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedData, setSelectedData] = useState(null);
+
     const handleMouseEnter = (category, index) => {
         setActiveLiIndex(`${category}-${index}`);
         setAnimationKey(prevKey => prevKey + 1);
@@ -13,44 +18,55 @@ const LiData = ({ category, dataObject, activeLiIndex, setActiveLiIndex, setAnim
         setActiveLiIndex(null);
     }
 
-    return dataObject.map((data, index) => (
-        <li
-            key={`${category}-${index + 1}`}
-            id={`${category}-${index + 1}`}
-            onMouseEnter={() => handleMouseEnter(category, index)}
-            onMouseLeave={handleMouseLeave}
-            className={activeLiIndex === `${category}-${index}` ? 'active' : ''}
-        >
-            <Link to={{ pathname: "/portfolio", state: data }}>
-                <div className='project-row'>
-                    <div className='project-row-left'>
-                        <div className='project-selected-wrapper'>
-                            {
-                                <FontAwesomeIcon
-                                    icon={faLongArrowAltRight}
-                                    className='project-selected'
-                                    style={{
-                                        transition: 'transform .3s ease-in-out',
-                                        transform: activeLiIndex === `${category}-${index}` ? 'translateX(0) translateZ(0)' : 'translateX(-100%) translateZ(0)'
-                                    }}
-                                />
-                            }
+    const handleLiClick = (data, category) => {
+        setSelectedData({ data, category });
+        setIsModalOpen(true);
+    }
+
+    return (
+        <>
+            {isModalOpen && <Modal selectedData={selectedData} onClose={() => setIsModalOpen(false)} />}
+            {dataObject.map((data, index) => (
+                <li
+                    key={`${category}-${index + 1}`}
+                    id={`${category}-${index + 1}`}
+                    onMouseEnter={() => handleMouseEnter(category, index)}
+                    onMouseLeave={handleMouseLeave}
+                    onClick={() => handleLiClick(data, category)}
+                    className={activeLiIndex === `${category}-${index}` ? 'active' : ''}
+                >
+                    <Link to={{ pathname: "/portfolio", state: data }}>
+                        <div className='project-row'>
+                            <div className='project-row-left'>
+                                <div className='project-selected-wrapper'>
+                                    {
+                                        <FontAwesomeIcon
+                                            icon={faLongArrowAltRight}
+                                            className='project-selected'
+                                            style={{
+                                                transition: 'transform .3s ease-in-out',
+                                                transform: activeLiIndex === `${category}-${index}` ? 'translateX(0) translateZ(0)' : 'translateX(-100%) translateZ(0)'
+                                            }}
+                                        />
+                                    }
+                                </div>
+                                <p style={{
+                                    transition: 'all .3s ease-in-out',
+                                    paddingLeft: activeLiIndex === `${category}-${index}` ? '20px' : '0',
+                                    fontWeight: activeLiIndex === `${category}-${index}` ? 'bold' : 'initial'
+                                }}>
+                                    {data.name}
+                                </p>
+                            </div>
+                            <div className='project-row-right'>
+                                <p>{data.skills[0]}</p>
+                            </div>
                         </div>
-                        <p style={{
-                            transition: 'all .3s ease-in-out',
-                            paddingLeft: activeLiIndex === `${category}-${index}` ? '20px' : '0',
-                            fontWeight: activeLiIndex === `${category}-${index}` ? 'bold' : 'initial'
-                        }}>
-                            {data.name}
-                        </p>
-                    </div>
-                    <div className='project-row-right'>
-                        <p>{data.skills[0]}</p>
-                    </div>
-                </div>
-            </Link>
-        </li >
-    ))
+                    </Link>
+                </li>
+            ))}
+        </>
+    )
 }
 
 export default LiData;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,58 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Modal.scss';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 
-const Modal = () => {
+const Modal = ({ selectedData, onClose }) => {
+    const { data, category } = selectedData;
+
+    let descriptionText = data.description.text;
+    let descriptionLi = data.description.li;
+
+    console.log(descriptionLi[0].content !== "");
+
+    let categoryName = category;
+    switch (categoryName) {
+        case "personal":
+            categoryName = "personnel";
+            break;
+
+        case "openclassrooms":
+            categoryName = "OpenClassrooms";
+            break;
+
+        default:
+            break;
+    }
+
     return (
-        <></>
+        <div className='modal-wrapper'>
+            <div className='modal'>
+                <button onClick={onClose} className='close-button'>
+                    <FontAwesomeIcon icon={faClose} />
+                </button>
+
+                {/* <img src={require(`../../${data.imageURL}`)} alt='Image projet' /> */}
+
+                <h2>{data.name}</h2>
+
+                <div>
+                    <p>{descriptionText}</p>
+
+                    {
+                        descriptionLi.content !== "" ?
+                            <ul>
+                                <li>test</li>
+                            </ul>
+                            : null
+                    }
+                </div>
+
+                <div>
+                    <h3>Catégorie</h3>
+                    <p>{categoryName}</p>
+                </div>
+            </div>
+        </div>
     )
 };
 
