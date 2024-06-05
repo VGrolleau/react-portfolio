45e68fc7f1ab7a2b460a6e7aace0dc3bd6c04b73
VGrolleau <virginie.grolleau+github@protonmail.com>
2024-06-05 15:34:04 +0200

    Manage Loader translation

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,14 @@
+import { useTranslation } from 'react-i18next';
 import './Loader.scss';
 
 /**
  * The Loader component in JavaScript React displays a preloader with a progress bar and text
- * indicating "Travail en cours" (Work in progress).
+ * indicating "Travail en cours" or "Work in progress" depending on language.
  * @returns The Loader component is being returned. It is a functional component that renders a
  * preloader with a custom SVG animation and a progress bar.
  */
 const Loader = () => {
+    const { t } = useTranslation();
     return (
         <div className='preloader'>
             <div className='inner'>
@@ -15,7 +17,7 @@ const Loader = () => {
                     <text x="14" y="39" fontFamily="Coolvetica" fontSize="60" fill="#115173" className='text-v'>V</text>
                     <text x="3" y="60" fontFamily="Coolvetica" fontSize="18" fill="white" stroke="none" className='text-virginie'>Virginie</text>
                 </svg>
-                <span>Travail en cours</span>
+                <span>{t('loader.work')}</span>
                 <div className='progress-bar' data-testid='progress-bar'>
                     <div className='progress-bar_bg'>
                         <div></div>
